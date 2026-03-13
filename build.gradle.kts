41565935cd6262029927313891c13b043e402b67
MinhLT <kaito1412.1102@gmail.com>
2026-03-13 21:44:50 +0700

    add android ci lint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,11 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.kotlin.detekt) apply false
 }
+
+/*
+detekt {
+    config.setFrom("$rootDir/config/detekt/detekt.yml")
+    buildUponDefaultConfig = true
+}*/
